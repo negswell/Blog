b081d66c72fb6a9764c5dfed8bb300ac02f8a5a7
unknown <negiaditya16@gmail.com>
2021-06-13 22:17:38 +0530

    update post api added

---
+++ src/utils/updatePost.ts
@@ -0,0 +1,27 @@
+import { basePath, secretKey } from './constants';
+
+const updatePost = async (id: string, title: string, body: string) => {
+  const response = await fetch(basePath, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-hasura-admin-secret': secretKey,
+    },
+    body: JSON.stringify({
+      query: `mutation MyMutation {
+  update_posts_by_pk(pk_columns: {id: "${id}"}, _set: {body: "${encodeURIComponent(
+        body
+      )}", title: "${encodeURIComponent(title)}"}) {
+    id
+  }
+}
+
+`,
+      variables: {},
+    }),
+  });
+
+  return await response.json();
+};
+
+export default updatePost;
