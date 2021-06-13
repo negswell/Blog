e777aa7ddee7b2cd8fa31f2ad92675bd5ced7941
unknown <negiaditya16@gmail.com>
2021-06-13 22:16:55 +0530

    delete post api added

---
+++ src/utils/deletePost.ts
@@ -0,0 +1,25 @@
+import { basePath, secretKey } from './constants';
+
+const deletePost = async (id: string) => {
+  const response = await fetch(basePath, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-hasura-admin-secret': secretKey,
+    },
+    body: JSON.stringify({
+      query: `mutation MyMutation {
+  delete_posts_by_pk(id: "${id}") {
+    id
+  }
+}
+
+`,
+      variables: {},
+    }),
+  });
+
+  return await response.json();
+};
+
+export default deletePost;
