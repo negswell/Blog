9879fbc7920c22c1579b987cf71adcbd6dfdc9ae
unknown <negiaditya16@gmail.com>
2021-06-13 22:38:04 +0530

    added blog container component

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
+import getPosts from '../utils/getPosts';
 import CreatePost from './CreatePost';
+import PostList from './PostList';
 
 const Blog: React.FC = () => {
+  const [data, setData] = React.useState<IPost[]>([]);
+  const [dataLoading, setDataLoading] = React.useState<boolean>(true);
+
+  React.useEffect(() => {
+    getPosts()
+      .then((post) => {
+        setData(post);
+      })
+      .finally(() => setDataLoading(false));
+  }, []);
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        padding: '16px',
-      }}
-    >
-      <CreatePost />
-    </div>
+    <>
+      <div
+        style={{
+          width: '100%',
+          position: 'sticky',
+          top: 0,
+          height: '70px',
+          zIndex: 1,
+          background: 'white',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <CreatePost setData={setData} setDataLoading={setDataLoading} />
+      </div>
+      <PostList
+        data={data}
+        dataLoading={dataLoading}
+        setDataLoading={setDataLoading}
+        setData={setData}
+      />
+    </>
   );
 };
 
