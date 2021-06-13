8ccb777ba604cc6f090ce12e392a4b868c459786
unknown <negiaditya16@gmail.com>
2021-06-13 22:24:38 +0530

    create post ui made

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,41 +1,27 @@
-import { Modal, Input, Form } from 'antd';
 import React from 'react';
 
-const CreatePost: React.FC = () => {
-  const [modalVisile, setModalVisile] = React.useState(false);
-  const [form] = Form.useForm();
+import PostForm from './PostForm';
 
-  const handleFinish = (values: ICreatePostForm) => {
-    console.log(values);
-  };
+interface IProps {
+  setData: (data: IPost[]) => void;
+  setDataLoading: (loading: boolean) => void;
+}
+
+const CreatePost: React.FC<IProps> = ({ setData, setDataLoading }) => {
+  const [modalVisible, setModalVisible] = React.useState<boolean>(false);
 
   return (
     <>
-      <div className='create-post' onClick={() => setModalVisile(true)}>
-        What's on your mind ?
+      <div className='create-post' onClick={() => setModalVisible(true)}>
+        What's on your mind ? Click here to share !
       </div>
-      <Modal
-        title='Create Post'
-        visible={modalVisile}
-        onOk={() => form.submit()}
-        onCancel={() => setModalVisile(false)}
-        destroyOnClose={true}
-      >
-        <Form wrapperCol={{ span: 24 }} form={form} onFinish={handleFinish}>
-          <Form.Item
-            name='title'
-            rules={[{ required: true, message: 'Please enter a title ' }]}
-          >
-            <Input placeholder='Title' />
-          </Form.Item>
-          <Form.Item
-            name='body'
-            rules={[{ required: true, message: 'Please enter a message' }]}
-          >
-            <Input.TextArea placeholder='What is on your mind ?' rows={8} />
-          </Form.Item>
-        </Form>
-      </Modal>
+      <PostForm
+        modalVisible={modalVisible}
+        setModalVisile={setModalVisible}
+        setData={setData}
+        setDataLoading={setDataLoading}
+        action='create'
+      />
     </>
   );
 };
