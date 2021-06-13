beee7b34af1f118eefbf86cecf6b8d6f9b11d997
unknown <negiaditya16@gmail.com>
2021-06-13 22:10:14 +0530

    added edit and delete buttons

---
+++ src/Widgets/Button/Edit.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button, Tooltip } from 'antd';
+import { EditFilled } from '@ant-design/icons';
+
+interface IProps {
+  title: string;
+  onClick: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+}
+
+/** Edit */
+const Edit = (props: IProps) => {
+  const { title, onClick } = props;
+
+  return (
+    <Tooltip title={title}>
+      <Button shape='circle' onClick={onClick}>
+        <EditFilled style={{ color: 'green' }} />
+      </Button>
+    </Tooltip>
+  );
+};
+
+Edit.defaultProps = {
+  title: 'Edit',
+};
+
+export default Edit;
