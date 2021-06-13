618a9f526a3dae21f18dfeeb515b5ad75f63a2ee
unknown <negiaditya16@gmail.com>
2021-06-13 22:17:14 +0530

    get posts api added

---
+++ src/utils/getPosts.ts
@@ -0,0 +1,28 @@
+import { basePath, secretKey } from './constants';
+
+const getPosts = async () => {
+  const response = await fetch(basePath, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-hasura-admin-secret': secretKey,
+    },
+    body: JSON.stringify({
+      query: `query MyQuery {
+  posts {
+    body
+    title
+    id
+  }
+}
+`,
+      variables: {},
+    }),
+  });
+
+  const result = await response.json();
+
+  return result.data.posts;
+};
+
+export default getPosts;
